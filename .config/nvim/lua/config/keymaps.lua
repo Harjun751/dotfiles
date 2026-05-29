fb0a95ba69f56c07311a2657da914a413ce1e252
Arjun Singh <Harvinder_Arjun_SUKHWANT_SINGH_from.TP@lta.gov.sg>
2026-05-29 17:41:44 +0800

    Add nvim and tmux configs

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+vim.keymap.set("n", "<leader>dT", function()
+  require("dap").disconnect({ terminateDebuggee = true })
+  require("dap").terminate({ all = true })
+  require("dap").close()
+end, { desc = "Hard terminate" })
